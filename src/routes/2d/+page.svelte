bf31e2a76e96741d34919ffe716bfd3efe38a055
Drew C Youngren <dcy2@columbia.edu>
2025-03-01 11:02:40 -0500

    Draw field

--- src/routes/2d/+page.svelte
+++ src/routes/2d/+page.svelte
@@ -1,48 +1,43 @@
-<script>
+<script lang="ts">
 	import { norm } from '$lib/mathutils';
 	import { arrow } from './SVGSnippets.svelte';
 
-	let puck = $state({
+	let proton = $state({
 		x: 0,
 		y: 0
 	});
 
-	$inspect(puck);
-	/**
-	 * @type SVGElement
-	 */
-	let svg;
+	$inspect(proton);
 
-	/**
-	 * @type SVGElement
-	 */
-	let puckElement;
+	let svg: SVGElement;
 
-	/**
-	 *
-	 * @param {MouseEvent} e
-	 */
-	function onMouseDown(e) {
+	function onMouseDown(e: MouseEvent) {
 		onMouseMove(e);
 		svg?.addEventListener('mousemove', onMouseMove);
 	}
 
-	/**
-	 *
-	 * @param {MouseEvent} e
-	 */
-	function onMouseUp(e) {
+	function onMouseUp(e: MouseEvent) {
 		svg?.removeEventListener('mousemove', onMouseMove);
 	}
 
-	/**
-	 *
-	 * @param {MouseEvent} e
-	 */
-	function onMouseMove(e) {
+	function onMouseMove(e: MouseEvent) {
 		console.log(e.clientY, e.offsetY, svg.clientHeight);
-		puck.x = (e.offsetX / svg.clientWidth - 0.5) * 300;
-		puck.y = (e.offsetY / svg.clientHeight - 0.5) * 200;
+		proton.x = (e.offsetX / svg.clientWidth - 0.5) * 300;
+		proton.y = (e.offsetY / svg.clientHeight - 0.5) * 200;
+	}
+
+	// Field dimensions
+	const WIDTH = 300;
+	const HEIGHT = 200;
+	const STEP = 15;
+
+	type Point = [number, number];
+	const vfCoords: Point[] = [];
+
+	for (let i = 1; i <= WIDTH / STEP; i++) {
+		for (let j = 1; j <= HEIGHT / STEP; j++) {
+			vfCoords.push([i * STEP - Math.round(WIDTH / 2), j * STEP - Math.round(HEIGHT / 2)]);
+		}
 	}
 </script>
 
@@ -52,21 +47,20 @@
 	xmlns="http://www.w3.org/2000/svg"
 	width="600"
 	height="400"
-	viewBox="-150 -100 300 200"
+	viewBox="{-WIDTH / 2} {-HEIGHT / 2} {WIDTH} {HEIGHT}"
 	onmouseup={onMouseUp}
 >
-	<!-- A rectangle inside the coordinate range -->
-	<circle cx="0" cy="0" r="10" fill="pink" />
-	<circle cx={puck.x} cy={puck.y} r="10" fill="blue" onmousedown={onMouseDown} />
-	{#if norm(puck.x, puck.y) > 0}
+	{#each vfCoords as [x, y]}
 		{@render arrow(
-			puck.x,
-			puck.y,
-			-(20 * puck.x) / norm(puck.x, puck.y),
-			-(20 * puck.y) / norm(puck.x, puck.y),
-			20 / norm(puck.x, puck.y)
+			x,
+			y,
+			(10 * (x - proton.x)) / norm(x - proton.x, y - proton.y),
+			(10 * (y - proton.y)) / norm(x - proton.x, y - proton.y),
+			200 / Math.pow(norm(x - proton.x, y - proton.y), 2)
 		)}
-	{/if}
+	{/each}
+
+	<circle cx={proton.x} cy={proton.y} r="5" fill="blue" onmousedown={onMouseDown} />
 </svg>
 
 <style>
--- src/routes/2d/SVGSnippets.svelte
+++ src/routes/2d/SVGSnippets.svelte
@@ -1,19 +1,19 @@
-<script module>
+<script module lang="ts">
 	import { norm } from '$lib/mathutils';
 	export { arrow };
 
 	const INVSQRT2 = 1 / Math.sqrt(2);
 </script>
 
-{#snippet arrow(x, y, dx, dy, alpha = 1, arrowHeadSize = 4)}
+{#snippet arrow(x: number, y: number, dx: number, dy: number, alpha = 1, arrowHeadSize = 4)}
 	{#if norm(dx, dy) > 0}
 		<path
-			d={`          M${x},${y} 
-  L${x + dx},${y + dy} 
-  M${x + dx},${y + dy} 
-  L${x + dx - (arrowHeadSize * (dx * INVSQRT2 - dy * INVSQRT2)) / norm(dx, dy)},${y + dy - (arrowHeadSize * (dx * INVSQRT2 + dy * INVSQRT2)) / norm(dx, dy)} 
-  M${x + dx},${y + dy} 
-  L${x + dx - (arrowHeadSize * (dx * INVSQRT2 + dy * INVSQRT2)) / norm(dx, dy)},${y + dy - (arrowHeadSize * (-dx * INVSQRT2 + dy * INVSQRT2)) / norm(dx, dy)}
+			d={`M${x - dx / 2},${y - dy / 2} 
+  L${x + dx / 2},${y + dy / 2} 
+  M${x + dx / 2},${y + dy / 2} 
+  L${x + dx / 2 - (arrowHeadSize * (dx * INVSQRT2 - dy * INVSQRT2)) / norm(dx, dy)},${y + dy / 2 - (arrowHeadSize * (dx * INVSQRT2 + dy * INVSQRT2)) / norm(dx, dy)} 
+  M${x + dx / 2},${y + dy / 2} 
+  L${x + dx / 2 - (arrowHeadSize * (dx * INVSQRT2 + dy * INVSQRT2)) / norm(dx, dy)},${y + dy / 2 - (arrowHeadSize * (-dx * INVSQRT2 + dy * INVSQRT2)) / norm(dx, dy)}
   `}
 			stroke="black"
 			stroke-width="1"
