f0e70924eee2eb9097d9f677986bef14971aaa11
Drew C Youngren <dcy2@columbia.edu>
2025-03-01 19:33:36 -0500

    Make n particles

--- src/routes/2d/+page.svelte
+++ src/routes/2d/+page.svelte
@@ -1,35 +1,68 @@
 <script lang="ts">
-	import { norm } from '$lib/mathutils';
 	import { arrow } from './SVGSnippets.svelte';
 
-	let proton = $state({
-		x: 0,
-		y: 0
-	});
+	interface Particle {
+		x: number;
+		y: number;
+		charge: number;
+	}
 
-	$inspect(proton);
+	let particles: Particle[] = $state([
+		{
+			x: 0,
+			y: 0,
+			charge: 1,
+		},
+		{
+			x: 50,
+			y: 50,
+			charge: -1,
+		},
+		{
+			x: -50,
+			y: -70,
+			charge: -1,
+		},
+	]);
+
+	function vf(u: number, v: number) {
+		const vec = [0, 0];
+		for (const p of particles) {
+			const r = Math.hypot(u - p.x, v - p.y);
+			if (r == 0) {
+				return [Infinity, Infinity];
+			} else {
+				vec[0] += (p.charge * (u - p.x)) / Math.pow(r, 3);
+				vec[1] += (p.charge * (v - p.y)) / Math.pow(r, 3);
+			}
+		}
+		return vec;
+	}
 
 	let svg: SVGElement;
 
-	function onMouseDown(e: MouseEvent) {
-		onMouseMove(e);
-		svg?.addEventListener('mousemove', onMouseMove);
-	}
+	function onMouseDown(e: MouseEvent, particle: Particle) {
+		e.preventDefault();
+		console.log('offset:', e.offsetX, e.offsetY, 'client:', e.clientX, e.clientY);
 
-	function onMouseUp(e: MouseEvent) {
-		svg?.removeEventListener('mousemove', onMouseMove);
-	}
+		function onMouseMove(e: MouseEvent) {
+			// console.log(e.clientY, e.offsetY, svg.clientHeight, e.target);
+			particle.x = (Math.max(0, Math.min(1, e.offsetX / svg.clientWidth)) - 0.5) * WIDTH;
+			particle.y = (Math.max(0, Math.min(1, e.offsetY / svg.clientHeight)) - 0.5) * HEIGHT;
+		}
 
-	function onMouseMove(e: MouseEvent) {
-		console.log(e.clientY, e.offsetY, svg.clientHeight);
-		proton.x = (e.offsetX / svg.clientWidth - 0.5) * 300;
-		proton.y = (e.offsetY / svg.clientHeight - 0.5) * 200;
+		function onMouseUp() {
+			document.removeEventListener('mousemove', onMouseMove);
+		}
+
+		document.addEventListener('mousemove', onMouseMove);
+		document.addEventListener('mouseup', onMouseUp);
 	}
 
 	// Field dimensions
 	const WIDTH = 300;
 	const HEIGHT = 200;
-	const STEP = 15;
+	const STEP = 10;
 
 	type Point = [number, number];
 	const vfCoords: Point[] = [];
@@ -48,19 +81,26 @@
 	width="600"
 	height="400"
 	viewBox="{-WIDTH / 2} {-HEIGHT / 2} {WIDTH} {HEIGHT}"
-	onmouseup={onMouseUp}
 >
-	{#each vfCoords as [x, y]}
+	{#each vfCoords.map(([u, v]) => [u, v, ...vf(u, v)]) as [x, y, vx, vy]}
 		{@render arrow(
 			x,
 			y,
-			(10 * (x - proton.x)) / norm(x - proton.x, y - proton.y),
-			(10 * (y - proton.y)) / norm(x - proton.x, y - proton.y),
-			200 / Math.pow(norm(x - proton.x, y - proton.y), 2)
+			(10 * vx) / Math.hypot(vx, vy),
+			(10 * vy) / Math.hypot(vx, vy),
+			200 * Math.hypot(vx, vy),
 		)}
 	{/each}
 
-	<circle cx={proton.x} cy={proton.y} r="5" fill="blue" onmousedown={onMouseDown} />
+	{#each particles as particle}
+		<circle
+			cx={particle.x}
+			cy={particle.y}
+			r="5"
+			fill={particle.charge > 0 ? 'blue' : 'red'}
+			onmousedown={(e) => onMouseDown(e, particle)}
+		/>
+	{/each}
 </svg>
 
 <style>
